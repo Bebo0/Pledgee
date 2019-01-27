7b338257680d76c2c05cfa6ab316e5e38beb3560
Bebo Elhosary <beboelhosary@hotmail.com>
2019-01-26 22:03:21 -0800

    Add Login page

--- nwhacks-app/src/App.js
+++ nwhacks-app/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import Login from './containers/Login';
 
 class App extends Component {
   render() {
     return (
+      //Login
       <p>
             Hello World!
       </p>
